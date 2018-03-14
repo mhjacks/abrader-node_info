6eb01498d1c809665f33c943339a68f80bad3fff
Andrew Brader <abrader@gmail.com>
2018-03-14 12:42:10 -0400

    Fix dir create & cached cat facts

--- lib/facter/pe_node_info.rb
+++ lib/facter/pe_node_info.rb
@@ -1,35 +1,22 @@
 begin
-  require 'json'
-  require 'puppet/util/nc_https'
-  require 'puppet_x/node_manager/common'
-rescue LoadError
-  mod = Puppet::Module.find('node_manager', Puppet[:environment].to_s)
-  require File.join mod.path, 'lib/puppet/util/nc_https'
-  require File.join mod.path, 'lib/puppet_x/node_manager/common'
-end
+  cached_catalog_file = "#{Puppet[:client_datadir]}/catalog/#{Facter.value('fqdn')}.json"
+  cached_catalog_json = JSON.parse(File.read(cached_catalog_file))
 
-# Retrieves node information to be parsed by 3rd party products
-module Puppet::Parser::Functions
-  newfunction(:node_info, type: :rvalue) do |args|
-    node_name = args[0]
-    node_facts = args[1]
-    node_trusted = args[2]
-
-    ng = Puppet::Util::Nc_https.new
-    # groups = ng.get_groups
-
-    ngroups = []
-
-    # When querying a specific group
-    raw = ng.get_classified(node_name, false, node_facts, node_trusted)
-
-    Puppet::Type.type(:node_group)
-    Puppet::Type::Node_group::ProviderHttps.instances
-
-    raw['groups'].map do |group|
-      gindex = Puppet::Type::Node_group::ProviderHttps.get_name_index_from_id(group)
-      ngroups << Hash[$ngs[gindex]['name'] => $ngs[gindex]['id'].to_s]
+  Facter.add(:cached_catalog_classes) do
+    setcode do
+      cached_catalog_json['classes']
     end
-    ngroups.to_json
+  end
+
+  Facter.add(:cached_catalog_environment) do
+    setcode do
+      cached_catalog_json['environment']
+    end
+  end
+
+  Facter.add(:cached_catalog_version) do
+    setcode do
+      cached_catalog_json['version']
+    end
   end
 end
--- manifests/init.pp
+++ manifests/init.pp
@@ -7,9 +7,25 @@ class node_info (
 ){
   $node_groups = node_info($trusted['certname'], { 'fact' =>  $facts }, { 'trusted' => $trusted })
 
+  if $facts['kernel'] == 'windows' {
+    $facter_dir       = dirname($node_groups_file)
+    $facter_child_dir = 'C:/ProgramData/PuppetLabs/facter'
+  }
+  else {
+    $facter_dir        = dirname($node_groups_file)
+    $facter_parent_dir = '/etc/facter'
+  }
+
+  file { [$facter_dir, $facter_parent_dir] :
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    #recurse => true,
+  }
+
   file { $node_groups_file :
     ensure  => file,
     content => inline_template("{ \"lastrun_node_groups\": <%= @node_groups %> }\n"),
   }
 }
-
